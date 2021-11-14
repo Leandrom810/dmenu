6dc81146428d87e5130638cd27c0f1ec84dd34bb
Leandro Moreira <leandromoreiravaz@gmail.com>
2021-11-13 22:11:51 -0300

    pronto

--- config.h
+++ config.h
@@ -4,17 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 220;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {"Terminus:size=10"};
+static const char *fonts[] = {"Terminus:size=12"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
         /*     fg         bg       */
-        [SchemeNorm] = { "#bbbbbb", "#222229" },
+        [SchemeNorm] = { "#bbbbbb", "#111119" },
         [SchemeSel] = { "#eeeeee", "#444444" },
         [SchemeOut] = { "#000000", "#00ffff" },
 };
-
-static int centered = 0;                 /* -c option; centers dmenu on screen */
-static int min_width = 500;              /* minimum width when centered */
 
 /* Alpha Patch */
 static const unsigned int alphas[SchemeLast][2] = {
